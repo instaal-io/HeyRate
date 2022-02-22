caf21566a631cdf34533624227d099e1cb4c57f2
ahmmedrejowan <ahmmedrejowan@gmail.com>
2022-02-23 00:57:09 +0600

    basic functions

---
+++ app/src/main/java/io/instaal/apprate/MainActivity.java
@@ -0,0 +1,16 @@
+package io.instaal.apprate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
