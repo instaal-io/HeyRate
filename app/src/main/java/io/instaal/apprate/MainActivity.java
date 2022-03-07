ed270bec4ac1964b318d5a21fda759f46eff5710
ahmmedrejowan <ahmmedrejowan@gmail.com>
2022-03-07 13:17:18 +0600

    UI Creations

--- app/src/main/java/io/instaal/apprate/MainActivity.java
+++ app/src/main/java/io/instaal/apprate/MainActivity.java
@@ -15,20 +15,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         CardView default_theme = findViewById(R.id.simple_theme);
-        default_theme.setOnClickListener(view -> {
-            new HeyRate(this)
-                    .setTheme(HeyRate.SIMPLE_THEME)
-                    .setTitle("Do You Like This App?")
-                    .setDescription("Please Give Us a Review")
-                    .setFeedbackButtonText("Message")
-                    .setRateButtonText("Rate")
-                    .setFeedbackTitle("Message")
-                    .setCancelButtonText("No")
-                    .setSendButtonText("Done")
-                    .setEditTextHint("Write Your Feedback")
-                    .start();
+        default_theme.setOnClickListener(view ->
 
-        });
+        new HeyRate(this)
+                .setTheme(HeyRate.SIMPLE_THEME)
+                .setTitle("Do You Like This App?")
+                .setDescription("Please Give Us a Review")
+                .setFeedbackButtonText("Message")
+                .setRateButtonText("Rate")
+                .setFeedbackTitle("Message")
+                .setCancelButtonText("No")
+                .setSendButtonText("Done")
+                .setEditTextHint("Write Your Feedback")
+                .start());
 
         CardView mini_theme = findViewById(R.id.default_theme);
         mini_theme.setOnClickListener(view -> {
