231f4ebb5515fb764d8df1753d6624547adc79b8
ahmmedrejowan <ahmmedrejowan@gmail.com>
2022-03-14 15:29:14 +0600

    Functionality Changes

--- app/src/main/java/io/instaal/apprate/MainActivity.java
+++ app/src/main/java/io/instaal/apprate/MainActivity.java
@@ -31,7 +31,9 @@ public class MainActivity extends AppCompatActivity {
 
 
         CardView simple_theme = findViewById(R.id.advanced_theme);
-        simple_theme.setOnClickListener(view -> new HeyRate.AdvanceTheme(this).start());
+        simple_theme.setOnClickListener(view -> new HeyRate
+                .AdvanceTheme(this)
+                .start());
 
 
     }
